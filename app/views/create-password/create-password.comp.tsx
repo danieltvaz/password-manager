d5a74ab708ede6cda68ff42c067602c9b702347c
danieltvaz <danieltostes@live.com>
2022-06-01 23:52:37 -0300

    feat: Create signup screen

--- app/views/create-password/create-password.comp.tsx
+++ app/views/create-password/create-password.comp.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import UserDataForm from 'app/components/smart/user-data-form';
+import {Logo} from 'app/components/dumb/logo';
+import {Section, TextLink} from './styles';
+import {CreatePasswordProps} from './create-password.hook';
 
-export default function CreatePassword() {
+export default function CreatePassword({navigation}: CreatePasswordProps) {
   return (
     <View>
+      <Section>
+        <Logo />
+      </Section>
       <UserDataForm />
+      <Section>
+        <Text>
+          {'Já possui uma senha? '}
+          <TextLink onPress={() => navigation.navigate('Signin')}>
+            {'Acessar'}
+          </TextLink>
+        </Text>
+      </Section>
     </View>
   );
 }
--- app/views/create-password/create-password.hook.ts
+++ app/views/create-password/create-password.hook.ts
@@ -1,3 +1,12 @@
-export default function useCreatePassword(props) {
-  return {...props};
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from 'App';
+
+export type CreatePasswordProps = {
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+};
+
+export default function useCreatePassword(props: any) {
+  const navigation = useNavigation();
+  return {...props, navigation};
 }
