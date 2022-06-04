8cda6d0c20ebf47f7d616f266a7bc8776dc25df7
danieltvaz <danieltostes@live.com>
2022-06-04 17:40:04 -0300

    chore: Generic type for useStorage

--- app/hooks/useStorage.ts
+++ app/hooks/useStorage.ts
@@ -6,7 +6,9 @@ const storageInstance = new MMKV();
 
 type Scope = 'credentials';
 
-export default function useStorage<T>(scope: Scope) {
+export default function useStorage<T>(
+  scope: Scope,
+): [T | undefined, (value: T) => void] {
   const [storage, setStorage] = useMMKVObject<T>(scope, storageInstance);
 
   return [storage, setStorage];
