c12124fac5f1434a688bc627f3c5d1544b2f0ef3
danieltvaz <danieltostes@live.com>
2022-06-14 21:42:06 -0300

    refactor: Change style for TextInput comp

--- app/components/dumb/text-input/styles.ts
+++ app/components/dumb/text-input/styles.ts
@@ -3,5 +3,6 @@ import styled from 'styled-components/native';
 export const StyledTextInput = styled.TextInput`
   border-bottom-width: 1px;
   border-bottom-color: ${({theme}) => theme.text.inactive};
-  width: 200px;
+  width: 100%;
+  padding: 0;
 `;
