478739a98a829fb5fb7533b1dcf21c2d41cf89f7
danieltvaz <danieltostes@live.com>
2022-06-29 20:33:12 -0300

    feat: Add memoization to list item

--- app/components/dumb/vault-item/index.ts
+++ app/components/dumb/vault-item/index.ts
@@ -1,5 +1,6 @@
 import PasswordItem from './password-item.comp';
 import usePasswordItem from './password-item.hook';
 import {withHook} from 'app/hocs';
+import {memo} from 'react';
 
-export default withHook(PasswordItem, usePasswordItem);
+export default memo(withHook(PasswordItem, usePasswordItem));
